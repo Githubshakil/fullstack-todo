595f0dad9c13c73c0d7001bdf682e065d30e6fb8
Githubshakil <shakilmahmud007@gmail.com>
2025-09-10 20:23:43 +0600

    update jwt

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,21 @@
-let registrationController = (req, res) => {
-  res.send("ami registration");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs')
+
+const generateAccessToken = (user) => {
+  return jwt.sign({ id: user._id }, process.env.ACCESS_SCRECT, {
+    expiresIn: "15m",
+  });
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign({ id: user._id }, process.env.REFRESH_TOKEN, {
+    expiresIn: "365d",
+  });
+};
+
+let registrationController =async (req, res) => {
+  const {username, email,password} = req.body;
+  const hashed = await bcrypt.hash(password,10)
 };
 let loginController = (req, res) => {
   res.send("ami login");
